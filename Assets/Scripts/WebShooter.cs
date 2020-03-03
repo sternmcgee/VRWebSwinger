a497bae02668a1a88bfdb4990030b8346172ecdf
sternmcgee <sternmcgee@tamu.edu>
2020-03-03 00:20:51 -0600

    added webshooter script

---
+++ Assets/Scripts/WebShooter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class WebShooter : MonoBehaviour
+{
+    public Hand hand = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
